eb4de8528e3b1deef5b99c8ed3c1c18ba5786080
beatriz-penalva <biapenalva@gmail.com>
2024-09-29 17:22:23 -0300

    chore: add env variable to get service url

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-// TODO: Add env variable with URL
-const URL = 'http://localhost:3001';
-
 export const api = axios.create({
-    baseURL: URL
+    baseURL: import.meta.env.VITE_BLOGGING_API_URL
 });
