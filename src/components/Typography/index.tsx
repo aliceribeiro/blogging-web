46a702dc64685e427623e2e5c25997663dd24462
beatriz-penalva <biapenalva@gmail.com>
2024-09-29 18:12:19 -0300

    refactor: remove unnecessary code

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -5,11 +5,8 @@ import "./styles.css";
 enum TypographyVariantEnum {
     'paragraph-xsmall-regular' = 'paragraph-xsmall-regular',
     'paragraph-sm-regular' = 'paragraph-sm-regular',
-    'paragraph-sm-regular-erased' = 'paragraph-sm-regular-erased',
     'paragraph-sm-medium' = 'paragraph-sm-medium',
     'paragraph-lg-regular' = 'paragraph-lg-regular',
-    'sr-only' = 'sr-only',
-    'subtitle-regular' = 'subtitle-regular',
     'subtitle-medium' = 'subtitle-medium',
     'subtitle-bold' = 'subtitle-bold',
     'title-bold' = 'title-bold',
@@ -18,11 +15,8 @@ enum TypographyVariantEnum {
 export const TypographyVariant = {
     'paragraph-xsmall-regular': TypographyVariantEnum['paragraph-xsmall-regular'],
     'paragraph-sm-regular': TypographyVariantEnum['paragraph-sm-regular'],
-    'paragraph-sm-regular-erased': TypographyVariantEnum['paragraph-sm-regular-erased'],
     'paragraph-sm-medium': TypographyVariantEnum['paragraph-sm-medium'],
     'paragraph-lg-regular': TypographyVariantEnum['paragraph-lg-regular'],
-    'sr-only': TypographyVariantEnum['sr-only'],
-    'subtitle-regular': TypographyVariantEnum['subtitle-regular'],
     'subtitle-medium': TypographyVariantEnum['subtitle-medium'],
     'subtitle-bold': TypographyVariantEnum['subtitle-bold'],
     'title-bold': TypographyVariantEnum['title-bold'],
