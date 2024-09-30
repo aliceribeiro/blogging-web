8b37503e24781e226d6f5037fe216a366b81b6b3
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2024-09-30 20:28:16 -0300

    fix: :bug: improve modal toggle

--- src/components/DeletePostTemplate/index.tsx
+++ src/components/DeletePostTemplate/index.tsx
@@ -1,53 +1,60 @@
-import { useMemo } from 'react';
+import { Modal as BootstrapModal } from "bootstrap";
 
-import { Modal as BootstrapModal } from 'bootstrap';
-
+import { useMemo } from "react";
+import { useDeletePost } from "../../hooks/useDeletePost";
 import { Button } from "../Button";
 import { Modal } from "../Modal";
 import { Typography } from "../Typography";
-import { useDeletePost } from "../../hooks/useDeletePost";
 
 type DeletePostTemplateProps = {
-    postId: string | number;
+  postId: string | number;
 };
 
 export const DeletePostTemplate = ({ postId }: DeletePostTemplateProps) => {
-    const modalId = useMemo(() => (`modal-delete-post-${postId}`), [postId]);
-    const handleToggleModal = () => {
-        const modal = BootstrapModal.getOrCreateInstance(document.getElementById(modalId)!);
-        modal.toggle();
-    };
-
-    const { loading, deletePost } = useDeletePost({ id: postId, onToggleModal: handleToggleModal });
-
-    return (
-        <>
-            <Button onClick={handleToggleModal} variant="danger">
-                Excluir
-            </Button>
-            <Modal actions={<>
-                <Button
-                    loading={loading}
-                    onClick={() => void deletePost()}
-                    variant="danger"
-                >
-                    Excluir
-                </Button>
-                <Button
-                    disabled={loading}
-                    onClick={handleToggleModal}
-                    variant="secondary"
-                >
-                    Cancelar
-                </Button>
-            </>}
-                id={modalId}
-                title="Tem certeza que deseja excluir a publicação?"
-            >
-                <Typography component="p" variant="paragraph-lg-regular">
-                    Atenção, pois essa ação não poderá ser desfeita.
-                </Typography>
-            </Modal>
-        </>
+  const modalId = useMemo(() => `modal-delete-post-${postId}`, [postId]);
+  const handleToggleModal = () => {
+    const modal = BootstrapModal.getOrCreateInstance(
+      document.getElementById(modalId)!
     );
+    modal.toggle();
+  };
+
+  const { loading, deletePost } = useDeletePost({
+    id: postId,
+    onToggleModal: handleToggleModal,
+  });
+
+  return (
+    <>
+      <Button onClick={handleToggleModal} variant="danger">
+        Excluir
+      </Button>
+      <Modal
+        actions={
+          <>
+            <Button
+              loading={loading}
+              onClick={() => void deletePost()}
+              variant="danger"
+            >
+              Excluir
+            </Button>
+            <Button
+              disabled={loading}
+              onClick={handleToggleModal}
+              variant="secondary"
+            >
+              Cancelar
+            </Button>
+          </>
+        }
+        id={modalId}
+        title="Tem certeza que deseja excluir a publicação?"
+      >
+        <Typography component="p" variant="paragraph-lg-regular">
+          Atenção, pois essa ação não poderá ser desfeita.
+        </Typography>
+      </Modal>
+    </>
+  );
 };
--- src/hooks/useDeletePost.ts
+++ src/hooks/useDeletePost.ts
@@ -8,47 +8,46 @@ import { useSnackbar } from "./useSnackbar";
 import { Paths } from "../routes/paths";
 
 type UseDeleteParams = {
-    id: string | number;
-    onToggleModal: () => void;
+  id: string | number;
+  onToggleModal: () => void;
 };
 
 export const useDeletePost = ({ id, onToggleModal }: UseDeleteParams) => {
-    const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    const navigate = useNavigate();
-    const { errorHandler } = useErrorHandler();
-    const { token } = usePermission();
-    const { setSnackbar } = useSnackbar();
+  const navigate = useNavigate();
+  const { errorHandler } = useErrorHandler();
+  const { token } = usePermission();
+  const { setSnackbar } = useSnackbar();
 
-    const deletePost = async () => {
-        setLoading(true);
-        try {
-            await deletePostService(id, token);
+  const deletePost = async () => {
+    setLoading(true);
+    try {
+      await deletePostService(id, token);
 
-            setSnackbar({
-                closable: true,
-                message: 'Publicação excluída com sucesso.',
-                variant: 'success'
-            });
+      setSnackbar({
+        closable: true,
+        message: "Publicação excluída com sucesso.",
+        variant: "success",
+      });
 
-            navigate(Paths.BASE);
-        } catch (e: unknown) {
-            onToggleModal();
+      navigate(Paths.BASE);
+    } catch (e: unknown) {
+      errorHandler(e);
+      setSnackbar({
+        closable: true,
+        message:
+          "Não foi possível excluir a publicação. Por favor, tente novamente mais tarde.",
+        variant: "error",
+      });
+    } finally {
+      onToggleModal();
+      setLoading(false);
+    }
+  };
 
-            errorHandler(e);
-
-            setSnackbar({
-                closable: true,
-                message: 'Não foi possível excluir a publicação. Por favor, tente novamente mais tarde.',
-                variant: 'error'
-            });
-        } finally {
-            setLoading(false);
-        };
-    };
-
-    return {
-        loading,
-        deletePost,
-    };
+  return {
+    loading,
+    deletePost,
+  };
 };
